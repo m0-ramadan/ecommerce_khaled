cdd8bd0c402e3fa76b2db131ae54ce1056d30fe6
Mohamed55088 <mohamedramadenzaki@gmail.com>
2026-02-12 15:51:49 +0200

    .

--- app/Models/ProductOptions.php
+++ app/Models/ProductOptions.php
@@ -127,4 +127,98 @@ class ProductOptions extends Model
         $extra = $this->extra_data_array;
         return $extra['weight'] ?? null;
     }
+    public function parentDetail()
+    {
+        return $this->belongsTo(ProductOptions::class, 'depends_on_detail_id');
+    }
+
+
+
+    public function quantityTiers()
+    {
+        return $this->hasMany(ProductSizeTier::class, 'option_id');
+    }
+
+    public function relatedTiers()
+    {
+        return $this->hasMany(ProductSizeTier::class, 'related_option_id');
+    }
+
+    // Scopes
+    public function scopeMainOptions($query)
+    {
+        return $query->whereNull('depends_on_option_id');
+    }
+
+    public function scopeDependentOptions($query, $optionId = null)
+    {
+        $query = $query->whereNotNull('depends_on_option_id');
+
+        if ($optionId) {
+            $query->where('depends_on_option_id', $optionId);
+        }
+
+        return $query;
+    }
+
+    public function scopeQuantityOptions($query)
+    {
+        return $query->where('option_name', 'like', '%كمية%')
+            ->orWhere('option_name', 'like', '%عدد%')
+            ->orWhere('option_name', 'like', '%حبات%');
+    }
+
+    // Helper Methods
+    public function hasDependencies()
+    {
+        return !is_null($this->depends_on_option_id);
+    }
+
+    public function getDependencyChain()
+    {
+        $chain = [];
+        $current = $this;
+
+        while ($current->depends_on_option_id) {
+            $parent = $current->parentOption;
+            if ($parent) {
+                $chain[] = [
+                    'option' => $parent->option_name,
+                    'value' => $parent->option_value,
+                    'condition' => $current->dependency_condition
+                ];
+                $current = $parent;
+            } else {
+                break;
+            }
+        }
+
+        return array_reverse($chain);
+    }
+
+
+    public function dependentOptions()
+    {
+        return $this->hasMany(
+            self::class,
+            'depends_on_option_id'
+        );
+    }
+
+
+    public function parentOption()
+    {
+        return $this->belongsTo(
+            self::class,
+            'depends_on_option_id',
+            'id'
+        );
+    }
+    public function getAvailableQuantityTiers()
+    {
+        return ProductSizeTier::where('option_id', $this->id)
+            ->where('is_quantity_tier', true)
+            ->orderBy('quantity')
+            ->get();
+    }
 }
