d562253cd085edbe2cca6ac3cedccf4536c78601
Mohamed Ramadan <mohamedramadenzaki@gmail.com>
2026-02-23 10:55:35 +0200

    ContactUsRequest mail not require

--- app/Http/Requests/Website/ContactUsRequest.php
+++ app/Http/Requests/Website/ContactUsRequest.php
@@ -18,7 +18,7 @@ class ContactUsRequest extends FormRequest
             'first_name' => 'required|string|max:100',
             'last_name'  => 'required|string|max:100',
             'phone'      => 'required|string|max:20',
-            'email'      => 'required|email|max:150',
+            'email'      => 'nullable|email|max:150',
             'company'    => 'nullable|string|max:150',
             'message'    => 'required|string|max:2000',
         ];
